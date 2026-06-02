efbfe3076575ec9e68b72f1509d604065189933b
jyh <yehyeon70@gmail.com>
2026-06-02 15:37:03 +0900

    Swagger Bearer 인증 설정 및 JaCoCo 커버리지 플러그인 추가

--- backend/src/main/java/com/pokemo/config/OpenApiConfig.java
+++ backend/src/main/java/com/pokemo/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.pokemo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,12 @@ public class OpenApiConfig {
         .info(new Info()
             .title("Pokemo API")
             .version("v1")
-            .description("Pokemo backend API scaffold."));
+            .description("Pokemo backend API scaffold."))
+        .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+        .components(new Components()
+            .addSecuritySchemes("Bearer", new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
   }
 }
